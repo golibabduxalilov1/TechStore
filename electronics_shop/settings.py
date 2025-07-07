8b1792f9dc10ab6d98c13d01ac90cd44ecef9ddf
Tommy <you@example.com>
2025-07-07 15:14:27 +0400

    host

--- electronics_shop/settings.py
+++ electronics_shop/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = "django-insecure-bfw^5ib-7(#*a*_l3m@zxi#9w8$29j88l2@)337u8$1z2*^zyw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
 
 # Application definition
