7b72295bfdd53a07e9f6651c086412b2cf21a9c2
G'olib Abdukhalil <golib.abduxalilov.1@proton.me>
2025-08-05 12:56:22 +0500

    Update settings.py

--- electronics_shop/settings.py
+++ electronics_shop/settings.py
@@ -65,15 +65,10 @@ WSGI_APPLICATION = "electronics_shop.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "techstore_db",
-        "USER": "postgres",
-        "PASSWORD": "admin123",
-        "HOST": "localhost",
-        "PORT": "5432",
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
-
 
 
 # Password validation
