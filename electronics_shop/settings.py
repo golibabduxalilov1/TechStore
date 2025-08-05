0081bf7686cbc6dfd3bd3f8b9d64d6895252c684
G'olib Abdukhalil <golib.abduxalilov.1@proton.me>
2025-08-05 13:16:40 +0500

    Update settings.py

--- electronics_shop/settings.py
+++ electronics_shop/settings.py
@@ -12,9 +12,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-bfw^5ib-7(#*a*_l3m@zxi#9w8$29j88l2@)337u8$1z2*^zyw"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["16.170.201.53"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
